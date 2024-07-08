7da068645eea2e23e761e530a1e35818b1292284
NemanjaJocanovic <jocanovicnemanja@gmail.com>
2024-07-08 08:57:58 +0200

    Add PASETO and JWT tokens

---
+++ token/payload.go
@@ -0,0 +1,41 @@
+package token
+
+import (
+	"errors"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var ErrExpiredToken = errors.New("token has expired")
+var ErrInvalidToken = errors.New("token is invalid")
+
+type Payload struct {
+	ID        uuid.UUID `json:"id"`
+	Username  string    `json:"username"`
+	IssuedAt  time.Time `json:"issued_at"`
+	ExpiredAt time.Time `json:"expired_at"`
+}
+
+// NewPayload creates a new payload with the username and duration
+func NewPayload(username string, duration time.Duration) (*Payload, error) {
+	TokenID, err := uuid.NewRandom()
+	if err != nil {
+		return nil, err
+	}
+
+	payload := &Payload{
+		ID:        TokenID,
+		Username:  username,
+		IssuedAt:  time.Now(),
+		ExpiredAt: time.Now().Add(duration),
+	}
+	return payload, nil
+}
+
+func (p *Payload) Valid() error {
+	if time.Now().After(p.ExpiredAt) {
+		return ErrExpiredToken
+	}
+	return nil
+}
